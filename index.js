9f6b3a46c9257202e9be4e12b077dc3df3e8f592
Nasrddinovvv1508 <najmiddinnasriddinov5@gmail.com>
2023-12-28 20:03:26 +0500

    All done

---
+++ index.js
@@ -0,0 +1,82 @@
+        // 1-MASALA
+// let str = `salom`;
+
+// let str1 = Array.from(str).reverse().join('');
+// console.log(str1);
+
+
+
+        // 2-MASALA
+// let str = `salom`;
+// let counter = 0;
+
+// let str1 = str.split('');
+
+// str1.forEach(v => {
+//     if (v == `a`) {
+//         counter++;
+//     }
+
+//     if (v == `o`) {
+//         counter++;
+//     }
+
+//     if (v == `u`) {
+//         counter++;
+//     }
+
+//     if (v == `i`) {
+//         counter++;
+//     }
+
+//     if (v == `e`) {
+//         counter++;
+//     }
+// });
+// console.log(counter);
+
+
+
+
+        // 3-MASALA
+// let str = 'radar';
+
+// let str1 = Array.from(str).reverse().join('');
+
+// if (str1 == str) {
+//     console.log("Bu so'z palindrom");
+// } else {
+//     console.log(`Bu so'z palindrom emas`);
+// }
+
+
+            // 4chi va 5chi masalani ishlolmadm((;
+
+
+        // 6-MASALA
+// let str = `aaaabbbcdd`;
+// let counterA = 0;
+// let counterB = 0;
+// let counterC = 0;
+// let counterD = 0;
+// let str_arr = str.split('');
+
+// str_arr.forEach((v) => {
+//     if (v == `a`) {
+//         counterA++;
+//     }
+
+//     if (v == 'b') {
+//         counterB++;
+//     }
+
+//     if (v == `c`) {
+//         counterC++;
+//     }
+
+//     if (v == 'd') {
+//         counterD++;
+//     }
+// })
+
+// console.log('a', counterA, 'b', counterB, 'c', counterC, 'd', counterD);
